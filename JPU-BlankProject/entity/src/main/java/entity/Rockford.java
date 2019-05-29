6d5c31c355b2cb2ba741b5a77b8e5e02e66011a7
VernierNicolas <45039057+VernierNicolas@users.noreply.github.com>
2019-05-29 14:22:42 +0200

    Cave(perso_mauvais_spwan)

--- JPU-BlankProject/entity/src/main/java/entity/Rockford.java
+++ JPU-BlankProject/entity/src/main/java/entity/Rockford.java
@@ -18,7 +18,7 @@ public class Rockford extends Cave {
 	private Image imgRight;
 	
 	public Rockford(int x, int y)  {
-		this.p = new Point(x, y);
+		this.p = new Point(x,y);
 		try {
 			this.imgUp = ImageIO.read(new File("D:\\images\\up.png"));
 		} catch (IOException e) {
